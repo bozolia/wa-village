4a076e1df0e3e2ea73c99680f70306bb8666c952
vincent.godin <vincent.godin@veolia.com>
2022-02-21 13:41:41 +0100

    first commit

---
+++ gothic.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="2021.01.13" name="gothic" tilewidth="32" tileheight="32" tilecount="1440" columns="16">
+ <image source="gothic.png" width="540" height="2902"/>
+</tileset>
