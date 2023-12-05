c8338c7949c0222c851affb2373a21bac5f2e40b
vincent godin <vincent.godin@veolia.com>
2023-12-05 21:54:17 +0100

    add area

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-WA.chat.sendChatMessage('Hello world', 'Mr Robot');
+const area = WA.room.area.create({
+    name: 'DangerArea',
+    x: 224,
+    y: 640,
+    width: 32,
+    height: 32,
+});
+
+const myAreaSubscriber = WA.room.area.onEnter("myArea").subscribe(() => {
+  WA.chat.sendChatMessage('Incident !!!! Incident !!!', 'ChatDEVOPS');
+  WA.room.showLayer("sky-danger");
+  WA.room.showLayer("ground-danger");
+});
+
+WA.room.area.onLeave("myArea").subscribe(() => {
+  WA.chat.sendChatMessage("Incident résolu !", "ChatDEVOPS");
+  WA.room.hideLayer("sky-danger");
+  WA.room.hideLayer("ground-danger");
+  myAreaSubscriber.unsubscribe();
+});
