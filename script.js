6f26ba102e404ca3829abf4ce849dfb520bd86e9
vincent godin <vincent.godin@veolia.com>
2023-12-05 22:38:57 +0100

    remove not working area, add menu

--- script.js
+++ script.js
@@ -1,20 +1,16 @@
-const area = WA.room.area.create({
-    name: 'DangerArea',
-    x: 224,
-    y: 640,
-    width: 32,
-    height: 32,
-});
-
-const myAreaSubscriber = WA.room.area.onEnter("myArea").subscribe(() => {
-  WA.chat.sendChatMessage('Incident !!!! Incident !!!', 'ChatDEVOPS');
-  WA.room.showLayer("sky-danger");
-  WA.room.showLayer("ground-danger");
-});
-
-WA.room.area.onLeave("myArea").subscribe(() => {
-  WA.chat.sendChatMessage("Incident résolu !", "ChatDEVOPS");
-  WA.room.hideLayer("sky-danger");
-  WA.room.hideLayer("ground-danger");
-  myAreaSubscriber.unsubscribe();
-});
+const menuIncident = WA.ui.registerMenuCommand('Incident',
+{
+  callback: () => {
+    WA.chat.sendChatMessage('Incident !!!! Incident !!!', 'ChatDEVOPS');
+    WA.room.showLayer("sky-danger");
+    WA.room.showLayer("ground-danger");
+  }
+})
+const menuFinIncident = WA.ui.registerMenuCommand('Fin Incident',
+{
+  callback: () => {
+    WA.chat.sendChatMessage("Incident résolu !", "ChatDEVOPS");
+    WA.room.hideLayer("sky-danger");
+    WA.room.hideLayer("ground-danger");
+  }
+})
