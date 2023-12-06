f558126c7b0b6a893a4981918bf6fded34cc055a
vincent godin <vincent.godin@veolia.com>
2023-12-06 10:06:27 +0100

    remove sky

--- script.js
+++ script.js
@@ -1,25 +1,21 @@
 const menuIncident = WA.ui.registerMenuCommand('Incident',
 {
   callback: () => {
-    WA.room.showLayer("sky-danger");
-    WA.room.showLayer("ground-danger");
-    WA.chat.sendChatMessage('Incident !', { uthor: 'ChatDEVOPS' });
+    WA.chat.sendChatMessage('Incident !', { author: 'ChatDEVOPS' });
   }
 })
 const menuFinIncident = WA.ui.registerMenuCommand('Fin Incident',
 {
   callback: () => {
-    WA.room.hideLayer("sky-danger");
-    WA.room.hideLayer("ground-danger");
-    WA.chat.sendChatMessage("Incident résolu !", { scope: 'local', author: 'ChatDEVOPS' });
+    WA.chat.sendChatMessage("Incident résolu !", { author: 'ChatDEVOPS' });
   }
 })
 
 WA.chat.onChatMessage((message => {
   if (message === 'Incident !') {
-    
+    WA.room.showLayer("ground-danger");
   }
   if (message === 'Incident résolu !') {
-  
+    WA.room.hideLayer("ground-danger");
   }
 }));
