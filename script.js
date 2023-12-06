bd80fdb4ad2ae21cde7d101675cc178e6de1111f
vincent godin <vincent.godin@veolia.com>
2023-12-06 09:51:52 +0100

    remove local option

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
 const menuIncident = WA.ui.registerMenuCommand('Incident',
 {
   callback: () => {
-    WA.chat.sendChatMessage('Incident !', { author: 'ChatDEVOPS' });
+    WA.room.showLayer("sky-danger");
+    WA.room.showLayer("ground-danger");
+    WA.chat.sendChatMessage('Incident !', { uthor: 'ChatDEVOPS' });
   }
 })
 const menuFinIncident = WA.ui.registerMenuCommand('Fin Incident',
 {
   callback: () => {
-    WA.chat.sendChatMessage("Incident résolu !", { author: 'ChatDEVOPS' });
+    WA.room.hideLayer("sky-danger");
+    WA.room.hideLayer("ground-danger");
+    WA.chat.sendChatMessage("Incident résolu !", { scope: 'local', author: 'ChatDEVOPS' });
   }
 })
 
 WA.chat.onChatMessage((message => {
   if (message === 'Incident !') {
-    WA.room.showLayer("sky-danger");
-    WA.room.showLayer("ground-danger");
-    return;
+    
   }
   if (message === 'Incident résolu !') {
-    WA.room.hideLayer("sky-danger");
-    WA.room.hideLayer("ground-danger");
+  
   }
 }));
