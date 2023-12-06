58573ee44ffa010ee4c6b2888af952c7b7f84399
vincent godin <vincent.godin@veolia.com>
2023-12-06 10:18:06 +0100

    use events with logs

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 const menuIncident = WA.ui.registerMenuCommand('Incident',
 {
   callback: () => {
+    console.log('debut incident');
     WA.event.broadcast("incident", "start");
   }
 })
 const menuFinIncident = WA.ui.registerMenuCommand('Fin Incident',
 {
   callback: () => {
+    console.log('fin incident')
     WA.event.broadcast("incident", "stop");
   }
 })
 
 WA.event.on("incident").subscribe((event) => {
+  console.log('incident', event.value);
   if (event.value === 'start') {
     WA.room.showLayer("ground-danger");
   }
