0fabb948122e3948954daf9956e20f854f90f786
vincent godin <vincent.godin@veolia.com>
2023-12-06 09:37:35 +0100

    chatMessage old signature

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 const menuIncident = WA.ui.registerMenuCommand('Incident',
 {
   callback: () => {
-    WA.chat.sendChatMessage('Incident !!!! Incident !!!', 'ChatDEVOPS');
-    WA.room.showLayer("sky-danger");
-    WA.room.showLayer("ground-danger");
+    WA.chat.sendChatMessage('Incident !', { scope: 'local', author: 'ChatDEVOPS' });
   }
 })
 const menuFinIncident = WA.ui.registerMenuCommand('Fin Incident',
 {
   callback: () => {
-    WA.chat.sendChatMessage("Incident résolu !", "ChatDEVOPS");
+    WA.chat.sendChatMessage("Incident résolu !", { scope: 'local', author: 'ChatDEVOPS' });
+  }
+})
+
+WA.chat.onChatMessage((message => {
+  if (message === 'Incident !') {
+    WA.room.showLayer("sky-danger");
+    WA.room.showLayer("ground-danger");
+    return;
+  }
+  if (message === 'Incident résolu !') {
     WA.room.hideLayer("sky-danger");
     WA.room.hideLayer("ground-danger");
   }
-})
+}));
